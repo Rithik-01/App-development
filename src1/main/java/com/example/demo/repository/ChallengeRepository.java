f1ca3a19a320976e63b220e1130277bb97d84d33
Rithik-01 <151500068+Rithik-01@users.noreply.github.com>
2024-08-09 16:22:16 +0530

    Delete src1/main/java/com/example/demo directory

--- src1/main/java/com/example/demo/repository/ChallengeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Challenge;
-
-public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
-    // Additional query methods can be defined here
-}
